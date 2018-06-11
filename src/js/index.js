cbd0e61e7af6d8385d68d3e30cf580856067cc8c
AxelE <axel.evrard@outlook.fr>
2018-06-11 11:03:30 +0200

    window resize

--- src/js/index.js
+++ src/js/index.js
@@ -118,3 +118,9 @@ function animate() {
 }
 animate();
 
+//window resize
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}, false);
